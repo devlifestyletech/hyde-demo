d52ec7c496e290428a61ae1b453d170c0d7470d2
dev-lifestyletech <85937790+psk-alphadev@users.noreply.github.com>
2022-01-27 17:11:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import SignInPage from "./apps/signin.page";
 import Loading from "./layout/Loading";
 import "./App.less";
 import "devextreme/dist/css/dx.light.css";
+import { encryptStorage } from "./utils/encryptStorage";
 require("dotenv").config();
 
 function App() {
 	const [session, setSession] = useState(false);
 	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const session = encryptStorage.getItem("user_session");
+		if (session) {
+			setSession(session);
+		}
+		setLoading(false);
+	}, []);
 
 	function useWindowSize() {
 		const [size, setSize] = useState([0, 0]);
@@ -16,16 +25,18 @@ function App() {
 			function updateSize() {
 				setSize([window.innerWidth, window.innerHeight]);
 			}
-			window.addEventListener('resize', updateSize);
+			window.addEventListener("resize", updateSize);
 			updateSize();
-			return () => window.removeEventListener('resize', updateSize);
+			return () => window.removeEventListener("resize", updateSize);
 		}, []);
 		return size;
 	}
 
 	const [width] = useWindowSize();
-	if (width <= 520) { return <>Please use desktop</> }
-	else {
+
+	if (width <= 520) {
+		return <>Please use desktop</>;
+	} else {
 		if (loading) {
 			return <Loading />;
 		}
@@ -34,8 +45,6 @@ function App() {
 		}
 		return <SignInPage />;
 	}
-
-
 }
 
 export default App;
