62b238970790f5e81b3b9357171fd2aa47a5df36
dev-lifestyletech <85937790+psk-alphadev@users.noreply.github.com>
2022-01-27 17:07:37 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import MainLayout from "./layout/MainLayout";
 import SignInPage from "./apps/signin.page";
 import Loading from "./layout/Loading";
 import "./App.less";
-import { encryptStorage } from "./utils/encryptStorage";
 import "devextreme/dist/css/dx.light.css";
-import { app } from "./utils/firebaseConfig";
-import { getAuth, signInAnonymously } from "firebase/auth";
 require("dotenv").config();
 
 function App() {
 	const [session, setSession] = useState(false);
 	const [loading, setLoading] = useState(true);
-
-	const auth = getAuth();
 
 	function useWindowSize() {
 		const [size, setSize] = useState([0, 0]);
@@ -29,23 +24,6 @@ function App() {
 	}
 
 	const [width] = useWindowSize();
-
-	useEffect(() => {
-		signInAnonymously(auth)
-			.then(() => {
-				console.log("Sign in with anonymously");
-			})
-			.catch((error) => {
-				const errorCode = error.code;
-				const errorMessage = error.message;
-			});
-		const session = encryptStorage.getItem("user_session");
-		if (session) {
-			setSession(session);
-		}
-		setLoading(false);
-	}, []);
-
 	if (width <= 520) { return <>Please use desktop</> }
 	else {
 		if (loading) {
