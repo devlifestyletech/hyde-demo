3f74cf86965978d236a0865bffd1a50d4536f34c
dev-lifestyletech <85937790+psk-alphadev@users.noreply.github.com>
2022-03-18 14:24:57 +0700

    change facility reservation firebase project and renew facility data

--- src/apps/facility_management/components/QrReservationModal.js
+++ src/apps/facility_management/components/QrReservationModal.js
@@ -1,15 +1,17 @@
-import React from "react";
-import QRCode from "qrcode.react";
-import { Modal, Row, Col, Button } from "antd";
-import { format } from "date-fns";
-import qrlogo from "../assets/hyde_2.svg";
+import React, { useRef } from 'react'
+import QRCode from 'qrcode.react'
+import { Modal, Row, Col, Button } from 'antd'
+import { format } from 'date-fns'
+import { ShareAltOutlined, DownloadOutlined } from '@ant-design/icons'
+import { exportComponentAsJPEG } from 'react-component-export-image'
 
 export default function QrReservationModal({ data, visible, onCancel }) {
+	const qrRef = useRef()
 	return (
 		<>
-			<Modal centered title="Reservation" visible={visible} onCancel={() => onCancel()} footer={null} width={400}>
-				<div style={{ textAlign: "center" }}>
-					<QRCode value={data?.id} size={200} />
+			<Modal centered title='Reservation' visible={visible} onCancel={() => onCancel()} footer={null} width={400}>
+				<div style={{ textAlign: 'center', width: 350, height: 350, backgroundColor: 'white', padding: 25 }} ref={qrRef}>
+					<QRCode value={data?.id} size={250} />
 				</div>
 				<div style={{ paddingLeft: 30, marginTop: 20 }}>
 					<Row>
@@ -23,7 +25,7 @@ export default function QrReservationModal({ data, visible, onCancel }) {
 					<Row>
 						<Col span={12}>Time :</Col>
 						<Col span={12}>
-							{data ? format(new Date(data?.startDateTime), "HH:mm") : null} - {data ? format(new Date(data?.endDateTime), "HH:mm") : null}
+							{data ? format(new Date(data?.startDateTime), 'HH:mm') : null} - {data ? format(new Date(data?.endDateTime), 'HH:mm') : null}
 						</Col>
 					</Row>
 					<Row>
@@ -43,12 +45,21 @@ export default function QrReservationModal({ data, visible, onCancel }) {
 						<Col span={12}>{data?.note}</Col>
 					</Row>
 				</div>
-				<div style={{ textAlign: "center" }}>
-					<Button shape="round" size="large" onClick={null} style={{ width: 200, marginTop: 50 }}>
+				<div style={{ textAlign: 'center' }}>
+					<Button shape='round' icon={<ShareAltOutlined />} size='large' onClick={null} style={{ width: 200, marginTop: 50 }}>
 						Share
+					</Button>
+					<Button
+						shape='round'
+						size='large'
+						onClick={() => exportComponentAsJPEG(qrRef, { fileName: data?.name + '-' + data?.id })}
+						style={{ width: 200, marginTop: 10 }}
+						icon={<DownloadOutlined />}
+					>
+						Save Image
 					</Button>
 				</div>
 			</Modal>
 		</>
-	);
+	)
 }
