8b8662e94dd61bc43b65d3be2aa1c3ea16c4bad6
dev-lifestyletech <85937790+psk-alphadev@users.noreply.github.com>
2022-03-21 10:02:59 +0700

    Update web-sockets.js

--- src/services/web-sockets.js
+++ src/services/web-sockets.js
@@ -1,12 +1,12 @@
-// import io from "socket.io-client";
-// let STRAPI_ENDPOINT;
+import io from 'socket.io-client'
+let STRAPI_ENDPOINT
 
-// if (process.env.NODE_ENV !== "production") {
-//   // STRAPI_ENDPOINT = "http://localhost:1337"; 
-//   STRAPI_ENDPOINT = 'https://hyde-dev.ngrok.io';
+if (process.env.NODE_ENV !== 'production') {
+	//   // STRAPI_ENDPOINT = "http://localhost:1337";
+	//   STRAPI_ENDPOINT = 'https://hyde-dev.ngrok.io';
+	STRAPI_ENDPOINT = 'https://artani-dev.ngrok.io'
+} else {
+	STRAPI_ENDPOINT = process.env.REACT_APP_API_URL
+}
 
-// } else {
-//   STRAPI_ENDPOINT = process.env.REACT_APP_API_URL;
-// }
-
-// export const socket = io(STRAPI_ENDPOINT);
+export const socket = io(STRAPI_ENDPOINT)
