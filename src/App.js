55d2bc7a2b50ab5c0e95481ed296cb8271694b9b
sarayut <sarayuthmoon@gmail.com>
2022-06-06 13:44:53 +0700

    6/6/2022
    compare file App, MainLayout, NoAuthLayout

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import 'devextreme/dist/css/dx.light.css';
 import { AuthProvider } from './hooks/useAuth';
 
 //Layout Component
-import NoAuth from './layout/NoAuthLayout';
+// import NoAuth from './layout/NoAuthLayout';
 import MainLayout from './layout/MainLayout';
-
+import NoAuthLayout from './layout/NoAuthLayout';
 // import UnauthorizeRoute from './routes/unauthorize_route';
 import SignInPage from './components/signin.page';
 import ResetPasswordPage from './components/reset_password.page';
 import ForgotPasswordPage from './components/forgot_password.page';
 import ConfirmRegistrationPage from './components/confirm_registration.page';
-
 // import AuthorizeRoute from './routes/authorize_route';
 import MainPage from './components/main.page';
 import FacilitiesManagementDashboardPage from './apps/facility_management/views/dashboard.page';
@@ -22,9 +21,9 @@ import BookingListsPage from './apps/facility_management/views/lists.page';
 import FacilitiesPage from './apps/facility_management/views/facilities.page';
 import OccupationPage from './apps/facility_management/views/occupation.page';
 import MemberDashboardPage from './apps/registration/views/RoomDashboard';
+import RoomDashboardPage from './apps/registration/views/RoomDashboard';
 import RegistrationPage from './apps/registration/views/Registration.page';
 import RoomManagement from './apps/registration/views/ProjectManagement';
-import RoomDashboardPage from './apps/registration/views/RoomDashboard';
 import Nearby from './apps/nearby/NearbyService';
 import Announcement from './apps/announcement/Announcement';
 import FixingReports from './apps/fixing_report/view/Fixing_Reports';
@@ -34,61 +33,142 @@ import LiveChat from './apps/live_chat/screens/ChatRoom';
 import PaymentSuccess from './apps/payment/Payment_success';
 import PaymentDashbord from './apps/payment/payment_dashbord';
 
-function App() {
+const App = () => {
   return (
     <AuthProvider>
       <Routes>
         {/* unauthorize_route */}
-        <Route element={<NoAuth />}>
-          <Route path="/" element={<SignInPage />} />
+        <Route element={<NoAuthLayout />}>
+          <Route index element={<SignInPage />} />
           <Route path="/signin" element={<SignInPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route
             path="/confirm-registration"
-            element={<ConfirmRegistrationPage />}
-          />
-        </Route>
+            element={<ConfirmRegistrationPage />}          
+	    />        	  
+	</Route>
         {/* authorize_route */}
-        <Route path="/dashboard" element={<MainLayout />}>
-          <Route path="summary" element={<MainPage />} />
+        <Route path='dashboard' element={<MainLayout />}>
+          <Route path='summary' element={<MainPage />} />
           <Route
-            path="facilities-reservation-dashboard"
+            path='facilities-reservation-dashboard'
             element={<FacilitiesManagementDashboardPage />}
           />
           <Route
-            path="facilities-reservation-calendar"
+            path='facilities-reservation-calendar'
             element={<BookingCalendarPage />}
           />
           <Route
-            path="facilities-reservation-list"
+            path='facilities-reservation-list'
             element={<BookingListsPage />}
           />
-          <Route path="facilities-management" element={<FacilitiesPage />} />
-          <Route path="occupation" element={<OccupationPage />} />
-          <Route path="members-dashboard" element={<MemberDashboardPage />} />
-          <Route path="members-registration" element={<RegistrationPage />} />
+          <Route path='facilities-management' element={<FacilitiesPage />} />
+          <Route path='occupation' element={<OccupationPage />} />
+          <Route path='members-dashboard' element={<MemberDashboardPage />} />
+          <Route path='members-registration' element={<RegistrationPage />} />
           <Route
-            path="rooms-management-dashboard"
+            path='rooms-management-dashboard'
             element={<RoomDashboardPage />}
           />
-          <Route path="rooms-management" element={<RoomManagement />} />
-          <Route path="nearby" element={<Nearby />} />
-          <Route path="announcement" element={<Announcement />} />
-          <Route path="service-center-reports" element={<FixingReports />} />
+          <Route path='rooms-management' element={<RoomManagement />} />
+          <Route path='nearby' element={<Nearby />} />
+          <Route path='announcement' element={<Announcement />} />
+          <Route path='service-center-reports' element={<FixingReports />} />
           <Route
-            path="service-center-dashboard"
+            path='service-center-dashboard'
             element={<FixingReportsDashboard />}
           />
-          <Route path="service-chat" element={<FixingChat />} />
-          <Route path="live-chat" element={<LiveChat />} />
-          <Route path="payment-billing" element={<PaymentSuccess />} />
-          <Route path="payment-dashboard" element={<PaymentDashbord />} />
+          <Route path='service-chat' element={<FixingChat />} />
+          <Route path='live-chat' element={<LiveChat />} />
+          <Route path='payment-billing' element={<PaymentSuccess />} />
+          <Route path='payment-dashboard' element={<PaymentDashbord />} />
         </Route>
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </AuthProvider>
   );
-}
+  // return (
+  //   <>
+  //     <h1>React Router</h1>
+  //     <Navigation />
+  //     <button onClick={() => authService.logout()}>Logout</button>
+  //     <Routes>
+  //       <Route element={<PublicRoute isSignIn={isSignIn} location={location} />}>
+  //         <Route index element={<SignInPage />} />
+  //         <Route path='/landing' element={<SignInPage />} />
+  //       </Route>
+  //       <Route element={<ProtectedRoute isSignIn={isSignIn} />}>
+  //         <Route path='/home' element={<Home />} />
+  //         <Route path='/dashboard' element={<Dashboard />} />
+  //         <Route path='/analytics' element={<Analytics />} />
+  //         <Route path='/admin' element={<Admin />} />
+  //       </Route>
+  //       <Route path='*' element={<p>There's nothing here: 404!</p>} />
+  //     </Routes>
+  //   </>
+  // );
+};
 
 export default App;
+
+//
+// const ProtectedRoute = ({
+//                           isSignIn,
+//                         }) => {
+//   if (!isSignIn) {
+//     return <Navigate to={'/landing'} />;
+//   }
+//   return <Outlet />;
+// };
+//
+//
+// const PublicRoute = ({
+//                        isSignIn,
+//                      }) => {
+//   if (!!isSignIn) {
+//     return <Navigate to={from !== "/" ? from : '/home'}/>;
+//     // return navigate(from);
+//   }
+//   return <Outlet />;
+// };
+// const Navigation = () => (
+//   <nav style={{ background: '#000' }}>
+//     <NavLink to='/landing'>Landing</NavLink>
+//     <NavLink to='/home'>Home</NavLink>
+//     <NavLink to='/dashboard'>Dashboard</NavLink>
+//     <NavLink to='/analytics'>Analytics</NavLink>
+//     <NavLink to='/admin'>Admin</NavLink>
+//   </nav>
+// );
+//
+//
+// const Landing = () => {
+//   return <h2>Landing (Public: anyone can access this page)</h2>;
+// };
+//
+// const Home = () => {
+//   return <h2>Home (Protected: authenticated user required)</h2>;
+// };
+//
+// const Dashboard = () => {
+//   return <h2>Dashboard (Protected: authenticated user required)</h2>;
+// };
+//
+// const Analytics = () => {
+//   return (
+//     <h2>
+//       Analytics (Protected: authenticated user with permission
+//       'analyze' required)
+//     </h2>
+//   );
+// };
+//
+// const Admin = () => {
+//   return (
+//     <h2>
+//       Admin (Protected: authenticated user with role 'admin' required)
+//     </h2>
+//   );
+// };
+
