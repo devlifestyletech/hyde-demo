606e8c165602eae8b62f3787c2d58ab06f26a916
psk-alphadev <85937790+psk2alphadev@users.noreply.github.com>
2022-09-08 12:07:21 +0700

    Update QrReservationModal.js

--- src/apps/facility_management/components/QrReservationModal.js
+++ src/apps/facility_management/components/QrReservationModal.js
@@ -9,85 +9,83 @@ import qrlogo from '../assets/hyde_2.svg';
 export default function QrReservationModal({data, visible, onCancel}) {
   const qrRef = useRef();
   return (
-      <>
-        <Modal
-            centered
-            title='Reservation'
-            visible={visible}
-            onCancel={() => onCancel()}
-            footer={null}
-            width={400}
+      <Modal
+          centered
+          title='Reservation'
+          visible={visible}
+          onCancel={() => onCancel()}
+          footer={null}
+          width={400}
+      >
+        <div
+            style={{
+              textAlign: 'center',
+              backgroundColor: 'white',
+              padding: 25,
+            }}
         >
-          <div
-              style={{
-                textAlign: 'center',
-                backgroundColor: 'white',
-                padding: 25,
+          <QRCode
+              ref={qrRef}
+              value={data?.id}
+              size={200}
+              imageSettings={{
+                src: qrlogo,
+                height: 40,
+                width: 40,
+                excavate: false,
               }}
+          />
+        </div>
+        <div style={{paddingLeft: 30, marginTop: 20}}>
+          <Row>
+            <Col span={12}>RoomName :</Col>
+            <Col span={12}>{data?.facility_name}</Col>
+          </Row>
+          <Row>
+            <Col span={12}>Topic :</Col>
+            <Col span={12}>{data?.topic}</Col>
+          </Row>
+          <Row>
+            <Col span={12}>Time :</Col>
+            <Col span={12}>
+              {data
+                  ? format(data?.startDateTime, 'HH:mm')
+                  : null} -{' '}
+              {data ? format(data?.endDateTime, 'HH:mm') : null}
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>Name - Surname :</Col>
+            <Col span={12}>{data?.name}</Col>
+          </Row>
+          <Row>
+            <Col span={12}>Telephone Number :</Col>
+            <Col span={12}>{data?.tel}</Col>
+          </Row>
+          <Row>
+            <Col span={12}>Number of People :</Col>
+            <Col span={12}>{data?.user_amount}</Col>
+          </Row>
+          <Row>
+            <Col span={12}>Note :</Col>
+            <Col span={12}>{data?.note}</Col>
+          </Row>
+        </div>
+        <div style={{textAlign: 'center'}}>
+          <Button
+              shape='round'
+              size='large'
+              onClick={() =>
+                  exportComponentAsJPEG(qrRef, {
+                    fileName: data?.name + '-' + data?.id,
+                  })
+              }
+              style={{width: 200, marginTop: 10}}
+              icon={<DownloadOutlined />}
           >
-            <QRCode
-                ref={qrRef}
-                value={data?.id}
-                size={200}
-                imageSettings={{
-                  src: qrlogo,
-                  height: 40,
-                  width: 40,
-                  excavate: false,
-                }}
-            />
-          </div>
-          <div style={{paddingLeft: 30, marginTop: 20}}>
-            <Row>
-              <Col span={12}>RoomName :</Col>
-              <Col span={12}>{data?.facility_name}</Col>
-            </Row>
-            <Row>
-              <Col span={12}>Topic :</Col>
-              <Col span={12}>{data?.topic}</Col>
-            </Row>
-            <Row>
-              <Col span={12}>Time :</Col>
-              <Col span={12}>
-                {data
-                    ? format(data?.startDateTime, 'HH:mm')
-                    : null} -{' '}
-                {data ? format(data?.endDateTime, 'HH:mm') : null}
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>Name - Surname :</Col>
-              <Col span={12}>{data?.name}</Col>
-            </Row>
-            <Row>
-              <Col span={12}>Telephone Number :</Col>
-              <Col span={12}>{data?.tel}</Col>
-            </Row>
-            <Row>
-              <Col span={12}>Number of People :</Col>
-              <Col span={12}>{data?.user_amount}</Col>
-            </Row>
-            <Row>
-              <Col span={12}>Note :</Col>
-              <Col span={12}>{data?.note}</Col>
-            </Row>
-          </div>
-          <div style={{textAlign: 'center'}}>
-            <Button
-                shape='round'
-                size='large'
-                onClick={() =>
-                    exportComponentAsJPEG(qrRef, {
-                      fileName: data?.name + '-' + data?.id,
-                    })
-                }
-                style={{width: 200, marginTop: 10}}
-                icon={<DownloadOutlined />}
-            >
-              Save Image
-            </Button>
-          </div>
-        </Modal>
-      </>
+            Save Image
+          </Button>
+        </div>
+      </Modal>
   );
 }
