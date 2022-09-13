b26872fac5274c3ddcef01047f340584a1f626ea
psk-alphadev <85937790+psk2alphadev@users.noreply.github.com>
2022-09-13 16:03:44 +0700

    update redux structure

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,20 @@
+const AuthState = {
+  isAuthenticated: false,
+  userData: null,
+  token: null,
+}
+
+const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_AUTH_STATE':
+      return {
+        ...state,
+        isAuthenticated: action.payload.isAuthenticated,
+        userData: action.payload.userData,
+        token: action.payload.token,
+      }
+    default:
+      return state
+  }
+}
+export { AuthReducer }
