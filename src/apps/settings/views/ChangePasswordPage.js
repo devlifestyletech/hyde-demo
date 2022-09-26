cb6a386811aacf334b3eb9d6e9355f812684b884
noppakornp <82705027+noppakornp@users.noreply.github.com>
2022-09-26 23:04:29 +0700

    validation strong password

--- src/apps/settings/views/ChangePasswordPage.js
+++ src/apps/settings/views/ChangePasswordPage.js
@@ -1,160 +1,167 @@
-import {Button, Col, Form, Image, Input, Modal} from 'antd';
-import axios from 'axios';
-import React, {useState} from 'react';
-import Header from '../../../components/Header';
-import {encryptStorage} from '../../../utils/encryptStorage';
-import LockImage from '../assets/images/change_password.png';
+import { Button, Col, Form, Image, Input, Modal } from 'antd'
+import axios from 'axios'
+import React, { useState } from 'react'
+import Header from '../../../components/Header'
+import { encryptStorage } from '../../../utils/encryptStorage'
+import LockImage from '../assets/images/change_password.png'
 
-import '../styles/profile.css';
+import '../styles/profile.css'
 
 function ChangePasswordPage() {
   // set static variables
-  const session = encryptStorage.getItem('user_session');
+  const session = encryptStorage.getItem('user_session')
   const header = {
     headers: {
       Authorization: 'Bearer ' + session.jwt,
     },
-  };
-  const URL = process.env.REACT_APP_API_URL;
-  const authURL = '/auth/local';
-  const changePassURL = `/users/${session.user.id}`;
+  }
+  const URL = process.env.REACT_APP_API_URL
+  const authURL = '/auth/local'
+  const changePassURL = `/users/${session.user.id}`
   // set variables
-  const [form] = Form.useForm();
-  const [disable, setDisable] = useState(false);
+  const [form] = Form.useForm()
+  const [disable, setDisable] = useState(false)
+  const strongPasswordRule = {
+    pattern: /^(?!.*\s)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
+    message:
+      'Password must 6 to 20 characters which contain at least must not contain whitespaces, one numeric digit, one uppercase and one lowercase letter',
+  }
 
   const onFinish = async (values) => {
     let value = {
       identifier: session.user.username,
       password: values.password,
-    };
-    console.log(value);
-    setDisable(true);
+    }
+    console.log(value)
+    setDisable(true)
 
     if (values.new_password === values.confirm_new_password) {
-      await axios.post(`${URL}${authURL}`, value).then(() => {
-        setDisable(false);
-        axios.put(
-            `${URL}${changePassURL}`,
-            {
-              password: values.new_password,
-            },
-            header,
-        ).then(() => {
-          form.setFieldsValue({
-            password: null,
-            new_password: null,
-            confirm_new_password: null,
-          });
-          Modal.success({
-            content: 'Change password success',
-          });
-        }).catch((err) => {
-          console.log(err);
-        });
-      }).catch(() => {
-        setDisable(false);
-        Modal.error({
-          title: 'Error !',
-          content: 'Your password is invalid, Please try again',
-        });
-      });
+      await axios
+        .post(`${URL}${authURL}`, value)
+        .then(() => {
+          setDisable(false)
+          axios
+            .put(
+              `${URL}${changePassURL}`,
+              {
+                password: values.new_password,
+              },
+              header
+            )
+            .then(() => {
+              form.setFieldsValue({
+                password: null,
+                new_password: null,
+                confirm_new_password: null,
+              })
+              Modal.success({
+                content: 'Change password success',
+              })
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+        })
+        .catch(() => {
+          setDisable(false)
+          Modal.error({
+            title: 'Error !',
+            content: 'Your password is invalid, Please try again',
+          })
+        })
     } else {
-      setDisable(false);
+      setDisable(false)
       Modal.error({
         title: 'Error !',
         content: 'Please confirm your new password again.',
-      });
+      })
     }
-  };
+  }
 
   return (
-      <>
-        <Header title='Change Password' />
-        <div className='profileContainer'>
-          <Col align='middle'>
-            <Col span={12}>
-              <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-              >
-                <Image preview={false} src={LockImage} />
-              </div>
-            </Col>
-            <Col className='spaceTopChangePass' span={12}>
-              <Form
-                  form={form}
-                  name='basic'
-                  onFinish={onFinish}
-                  autoComplete='off'
-                  layout='vertical'
-              >
-                <Col span={24}>
-                  <Col span={24}>
-                    <div
-                        style={{
-                          display: 'flex',
-                          flexDirection: 'column',
-                        }}
-                    >
-                      <Form.Item
-                          label='Confirm Your Existing Password'
-                          name='password'
-                          rules={[
-                            {
-                              required: true,
-                              message: 'Please input your password!',
-                            },
-                          ]}
-                      >
-                        <Input.Password />
-                      </Form.Item>
-                      <Form.Item
-                          label='New Password'
-                          name='new_password'
-                          rules={[
-                            {
-                              required: true,
-                              message: 'Please input your new password!',
-                            },
-                          ]}
-                      >
-                        <Input.Password />
-                      </Form.Item>
-                      <Form.Item
-                          label='Confirm New Password'
-                          name='confirm_new_password'
-                          rules={[
-                            {
-                              required: true,
-                              message: 'Please confirm your new password!',
-                            },
-                          ]}
-                      >
-                        <Input.Password />
-                      </Form.Item>
-                    </div>
-                  </Col>
-                  <Form.Item>
-                    <Button
-                        type='primary'
-                        size='large'
-                        shape='round'
-                        htmlType='submit'
-                        disabled={disable}
-                        className='changePassBtn'
-                    >
-                      Save
-                    </Button>
-                  </Form.Item>
-                </Col>
-              </Form>
-            </Col>
+    <>
+      <Header title="Change Password" />
+      <div className="profileContainer">
+        <Col align="middle">
+          <Col span={12}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Image preview={false} src={LockImage} />
+            </div>
           </Col>
-        </div>
-      </>
-  );
+          <Col className="spaceTopChangePass" span={12}>
+            <Form form={form} name="basic" onFinish={onFinish} autoComplete="off" layout="vertical">
+              <Col span={24}>
+                <Col span={24}>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                    }}
+                  >
+                    <Form.Item
+                      label="Confirm Your Existing Password"
+                      name="password"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please input your password!',
+                        },
+                      ]}
+                    >
+                      <Input.Password />
+                    </Form.Item>
+                    <Form.Item
+                      label="New Password"
+                      name="new_password"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please input your new password!',
+                        },
+                        strongPasswordRule,
+                      ]}
+                    >
+                      <Input.Password />
+                    </Form.Item>
+                    <Form.Item
+                      label="Confirm New Password"
+                      name="confirm_new_password"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please confirm your new password!',
+                        },
+                        strongPasswordRule,
+                      ]}
+                    >
+                      <Input.Password />
+                    </Form.Item>
+                  </div>
+                </Col>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    size="large"
+                    shape="round"
+                    htmlType="submit"
+                    disabled={disable}
+                    className="changePassBtn"
+                  >
+                    Save
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Form>
+          </Col>
+        </Col>
+      </div>
+    </>
+  )
 }
 
-export default ChangePasswordPage;
+export default ChangePasswordPage
